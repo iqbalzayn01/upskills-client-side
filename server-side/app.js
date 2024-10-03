c28418352bcb476d64fdd107fddbe90af7608dbc
Iqbal Zayyan <73063960+iqbalzayn01@users.noreply.github.com>
2024-10-03 12:11:06 +0700

    update: vercel.json v3

--- server-side/app.js
+++ server-side/app.js
@@ -45,18 +45,10 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-
-// app.get('/', (req, res) => {
-//   res.status(200).json({
-//     message: `Welcome to API UpSkills`,
-//   });
-// });
-
-// Render EJS view for the homepage
 app.get('/', (req, res) => {
-  res.render('index');
+  res.status(200).json({
+    message: `Welcome to API UpSkills`,
+  });
 });
 
 // App Router
